a00c21d13dffb2e9a27aee59369ee2872290cd1e
xtangxtang <xi.tang@intel.com>
2021-04-01 14:24:50 +0800

    add malloc internal

--- src/mstatics.c
+++ src/mstatics.c
@@ -2,26 +2,34 @@
 
 #include <stdio.h>
 #include <dlfcn.h>
+#include "mstatics.h"
 
 static void* (*real_malloc)(size_t)=NULL;
 
-static void mtrace_init(void)
-{
+static void mtrace_init(void) {
     real_malloc = dlsym(RTLD_NEXT, "malloc");
     if (NULL == real_malloc) {
         fprintf(stderr, "Error in `dlsym`: %s\n", dlerror());
     }
 }
 
-void *malloc(size_t size)
-{
+
+void *malloc(size_t size) {
     if(real_malloc==NULL) {
         mtrace_init();
     }
-
+    
     void *p = NULL;
-    fprintf(stderr, "malloc(%d)", size);
+    fprintf(stderr, "malloc(%d)\n", size);
     p = real_malloc(size);
-    fprintf(stderr, "%p\n", p);
+    //fprintf(stderr, "%p\n", p);
     return p;
 }
+
+void *malloc_internal(size_t size, char const * caller_name ) {
+    printf( "malloc was called from %s\n", caller_name );
+    malloc(size);
+}
+
+#define malloc(size) malloc_internal(size, __func__)
+
--- src/mstatics.h
+++ src/mstatics.h
@@ -1,28 +1,4 @@
-#define _GNU_SOURCE 
-#include <dlfcn.h>
-
-#include <stdlib.h>
 #include <stdio.h>
 
 
-static void* (*r_malloc)(size_t) = NULL;
-
-void initialize() {
-    r_malloc = dlsym(RTLD_NEXT, "malloc");
-}
-
-void *malloc_inner(size_t size, char const * caller_name ) {
-    printf( "a was called from %s\n", caller_name );
-    malloc(size);
-}
-
-#define malloc(size) malloc_inner(size, __func__)
-
-/* int main() {
-    int i=100;
-    char * buffer;
-
-    buffer = (char*) malloc (i+1);
-    if (buffer==NULL) exit (1);
-} */
 

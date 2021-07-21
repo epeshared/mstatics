8521f65358d4e99ea515d551cc77a121ca77c5dd
xtangxtang <xi.tang@intel.com>
2021-07-21 18:52:34 +0800

    update

--- src/processfile.py
+++ src/processfile.py
@@ -499,7 +499,8 @@ def process_memory_usage_file(pdwriter, inputPath):
                 'values':     [func, 1,index,row_num-1,index],
                 'data_labels': {'value': True}
             })
-            index = index + 1 
+            index = index + 1
+            line_chart.set_x_axis({'date_axis': True}) 
             function_sheet.insert_chart(3+20*(index - 1),col_num + 5, line_chart)
         
 
